5b8d9607c9a30bf232e503f85235797282a4fb05
ArlinTheWizard <alinxbs123@gmail.com>
2024-09-30 16:15:40 +0300

    Create pb4.cpp

---
+++ pb4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    char c[255];
+    cin.get(c,255);
+    int l=strlen(c);
+    for(int i=0;i<l;i++)
+    {
+        if(c[i]=='a' || c[i]=='e' || c[i]=='i' || c[i]=='o' || c[i]=='u' || c[i]=='A' || c[i]=='E' || c[i]=='I' || c[i]=='O' || c[i]=='U')
+        {
+            if(c[i]>='A' && c[i]<='Z')
+            {
+                c[i]+='a'-'A';
+            }
+        }
+        else
+        {
+            if(c[i]>='a' && c[i]<='z')
+            {
+                c[i]-='a'-'A';
+            }
+        }
+    }
+    cout << c;
+}
