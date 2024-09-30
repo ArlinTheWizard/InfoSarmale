3cc477285ee9b11983ae77981c9dbc8e9955a043
ArlinTheWizard <alinxbs123@gmail.com>
2024-09-30 15:55:08 +0300

    Update pb1.cpp

--- pb1.cpp
+++ pb1.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+#include <cstring>
 
+using namespace std;
+
+int main()
+{
+    char c[255];
+    char a;
+    cin >> a;
+    char s[10];
+    cin >> s;
+    cin.get();
+    cin.get(c,255);
+    int lenght=strlen(c);
+    int k1=0;
+    for(int i=0;i<lenght;i++)
+    {
+        if(a==c[i])
+        {
+            k1++;
+        }
+    }
+    int k2=0;
+    for(int i=0;i<lenght;i++)
+    {
+        if(c[i]=='a' || c[i]=='e' || c[i]=='i' || c[i]=='o' || c[i]=='u' || c[i]=='A' || c[i]=='E' || c[i]=='I' || c[i]=='O' || c[i]=='U')
+        {
+            k2++;
+        }
+    }
+    int k3=0;
+    int l=strlen(s);
+    for(int i=0;i<lenght-l+1;i++)
+    {
+        int ii=0;
+        bool b=true;
+        for(int j=i;j<i+l;j++)
+        {
+            if(c[j]!=s[ii])
+            {
+                b=false;
+            }
+            ii++;
+        }
+        if(b==true)
+        {
+            k3++;
+        }
+    }
+    cout << k1 << "\n" << k2 << "\n" << k3;
+}
